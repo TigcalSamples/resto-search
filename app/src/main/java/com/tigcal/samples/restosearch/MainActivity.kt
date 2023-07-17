3d9bd22de685560ceaf5ab0ec72628cfbf083e1e
Jomar Tigcal <jomar@tigcal.com>
2023-07-17 14:41:47 -0700

    Adding search menu action item to the main screen

--- app/src/main/java/com/tigcal/samples/restosearch/MainActivity.kt
+++ app/src/main/java/com/tigcal/samples/restosearch/MainActivity.kt
@@ -1,11 +1,40 @@
 package com.tigcal.samples.restosearch
 
+import android.app.SearchManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import androidx.appcompat.widget.SearchView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.menu_search, menu)
+
+        val manager = getSystemService(SEARCH_SERVICE) as SearchManager
+        val searchView = menu?.findItem(R.id.action_search)?.actionView as? SearchView
+        searchView?.let { searchVue ->
+            searchVue.setSearchableInfo(manager.getSearchableInfo(componentName))
+            searchVue.setOnQueryTextListener(object : SearchView.OnQueryTextListener {
+                override fun onQueryTextSubmit(query: String): Boolean {
+                    searchRestaurant(query)
+                    return true
+                }
+
+                override fun onQueryTextChange(newText: String): Boolean {
+                    return true
+                }
+            })
+        }
+
+        return super.onCreateOptionsMenu(menu)
+    }
+
+    private fun searchRestaurant(query: String) {
+        TODO("Not yet implemented")
     }
 }
