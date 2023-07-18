d79bdc8fbc1de472ba0f62c03dd7e3420664f730
Jomar Tigcal <jomar@tigcal.com>
2023-07-17 18:10:08 -0700

    Adding check for location permission and handling denial

--- app/src/main/java/com/tigcal/samples/restosearch/MainActivity.kt
+++ app/src/main/java/com/tigcal/samples/restosearch/MainActivity.kt
@@ -1,12 +1,24 @@
 package com.tigcal.samples.restosearch
 
+import android.Manifest
 import android.app.SearchManager
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.net.Uri
 import android.os.Bundle
+import android.provider.Settings
 import android.view.Menu
+import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.widget.SearchView
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+import com.google.android.material.dialog.MaterialAlertDialogBuilder
 
 class MainActivity : AppCompatActivity() {
+    private val REQUEST_CODE = 0
+
+    private var searchQuery = ""
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -34,7 +46,49 @@ class MainActivity : AppCompatActivity() {
         return super.onCreateOptionsMenu(menu)
     }
 
+    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<String>, grantResults: IntArray) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        when (requestCode) {
+            REQUEST_CODE -> if (grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                searchRestaurant(searchQuery)
+            } else {
+                MaterialAlertDialogBuilder(this)
+                    .setTitle(getString(R.string.app_name))
+                    .setMessage(getString(R.string.permission_needed))
+                    .setPositiveButton(getString(R.string.permission_okay)) { _, _ ->
+                        openSettingsPage()
+                    }.setNegativeButton(getString(R.string.permission_cancel), null)
+                    .create().show()
+            }
+        }
+    }
+
     private fun searchRestaurant(query: String) {
-        TODO("Not yet implemented")
+        searchQuery = query
+        if (!hasLocationPermission()) {
+            requestLocationPermission()
+        } else {
+            //TODO search now
+        }
+    }
+
+    private fun hasLocationPermission() = ContextCompat.checkSelfPermission(
+        this, Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED ||
+            ContextCompat.checkSelfPermission(this,
+                Manifest.permission.ACCESS_COARSE_LOCATION) == PackageManager.PERMISSION_GRANTED
+
+    private fun requestLocationPermission() {
+        ActivityCompat.requestPermissions(this,
+            arrayOf(Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.ACCESS_COARSE_LOCATION),
+            REQUEST_CODE)
+    }
+
+    private fun openSettingsPage() {
+        val intent = Intent(
+            Settings.ACTION_APPLICATION_DETAILS_SETTINGS,
+            Uri.fromParts("package", packageName, null)
+        )
+        intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
+        startActivity(intent)
     }
 }
